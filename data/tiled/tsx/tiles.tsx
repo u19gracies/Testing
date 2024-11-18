36361f8f8338043019b1f70bcb4afd0ceafe3edc
u19gracues <u19gracies@cfgslive.com>
2024-11-18 17:18:27 +0000

    image bugfix

--- data/tiled/tsx/tiles.tsx
+++ data/tiled/tsx/tiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="tiles" tilewidth="16" tileheight="16" tilecount="24" columns="8">
- <image source="../../../../../aseprite/tilemap.png" width="128" height="48"/>
+ <image source="../../images/tilemap.png" width="128" height="48"/>
  <tile id="0" type="Grass"/>
  <tile id="1" type="Grass"/>
  <tile id="2" type="Grass"/>
