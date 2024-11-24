3addf16e1cd3e4a326373e76537e00ba7841939d
u19gracues <u19gracies@cfgslive.com>
2024-11-24 17:22:29 +0000

    item update(unfinished)

--- SpriteHandler.py
+++ SpriteHandler.py
@@ -6,21 +6,40 @@ class Player(pygame.sprite.Sprite):
     self.scale = scale
     self.x = x
     self.y = y
-    self.image = pygame.image.load('data/images/basicCharacterSSheet.png').convert_alpha()
+    self.walkImage = pygame.image.load('data/images/basicCharacterSSheet.png')
+    self.swordWalkImage = pygame.image.load('data/images/swordDrag.png')
     self.shadow = pygame.image.load('data/images/playerShadow.png').convert_alpha()
-    self.image = pygame.transform.scale(self.image, (self.image.get_width()*self.scale,self.image.get_height()*self.scale))
+    self.attImage = pygame.image.load('data/images/swordAttack.png').convert_alpha()
+    self.walkImage = pygame.transform.scale(self.walkImage, (self.walkImage.get_width()*self.scale,self.walkImage.get_height()*self.scale))
+    self.swordWalkImage = pygame.transform.scale(self.swordWalkImage, (self.swordWalkImage.get_width()*self.scale,self.swordWalkImage.get_height()*self.scale))
     self.shadow = pygame.transform.scale(self.shadow, (self.shadow.get_width()*4,self.shadow.get_height()*4))
-    self.f = 1
-    self.playerSurf = pygame.Surface((32*self.scale,32*self.scale))
-    self.rect = self.playerSurf.get_rect()
+    self.attImage = pygame.transform.scale(self.attImage, (self.attImage.get_width()*4,self.attImage.get_height()*4))
     self.shadowrect = self.shadow.get_rect()
     self.shadowrect.center = (self.x, self.y+40)
+
+    self.playerSurf = pygame.Surface((32*self.scale,32*self.scale), pygame.SRCALPHA)
+    self.rect = self.playerSurf.get_rect()
     self.rect.center = (self.x, self.y)
 
-  def update(self, f, screen, orientation):
-    self.f = f
-    self.playerSurf = pygame.Surface((32*self.scale,32*self.scale), pygame.SRCALPHA)
-    self.playerSurf.blit(self.image, (0,0), (32*f*self.scale, 0, 32*self.scale, 32*self.scale))
+
+  def update(self, walkf, attf, screen, orientation, attacking, swordHeld):
+    ##update player
+    f = walkf
+    if swordHeld:
+      self.playerSurf = pygame.Surface((48*self.scale,48*self.scale), pygame.SRCALPHA)
+      if attacking:
+        f = attf
+        self.playerSurf.blit(self.attImage, (0,0), (48*f*self.scale, 0, 48*self.scale, 48*self.scale))
+      else:
+        self.playerSurf.blit(self.swordWalkImage, (0,0), (48*f*self.scale, 0, 48*self.scale, 48*self.scale))
+
+    else:
+      self.playerSurf = pygame.Surface((32*self.scale,32*self.scale), pygame.SRCALPHA)
+      self.playerSurf.blit(self.walkImage, (0,0), (32*f*self.scale, 0, 32*self.scale, 32*self.scale))
+
+    self.rect = self.playerSurf.get_rect()
+    self.rect.center = (self.x, self.y)
+
 
     screen.blit(self.shadow, self.shadowrect)
     if orientation == 2:
@@ -29,13 +48,14 @@ class Player(pygame.sprite.Sprite):
       screen.blit(self.playerSurf, self.rect)
 
 
-class Tile(pygame.sprite.Sprite):
-  def __init__(self,pos,surf,groups):
-    super().__init__(groups)
-    self.image = pygame.transform.scale(surf, (surf.get_width()*5,surf.get_height()*5))
-    self.rect = self.image.get_rect(topleft=pos)
 
-  def update(self, isMoving):
+class BG():
+  def __init__(self, surf):
+    self.image = pygame.transform.scale(surf, (surf.get_width()*5,surf.get_height()*5))
+    self.rect = self.image.get_rect(topleft=(0,0))
+    self.offset = pygame.Vector2(0,0)
+    
+  def update(self, isMoving, walkSpeed):
     vectorX = 0
     vectorY = 0
     if isMoving:
@@ -51,8 +71,36 @@ class Tile(pygame.sprite.Sprite):
         vectorX = -1
 
       if vectorX==0 and vectorY==0:
-        direction = pygame.Vector2(vectorX, vectorY) * 5
+        direction = pygame.Vector2(vectorX, vectorY) * walkSpeed
       else:
-        direction = pygame.Vector2(vectorX, vectorY).normalize() * 5
+        direction = pygame.Vector2(vectorX, vectorY).normalize() * walkSpeed
+
+      self.offset = direction
       
       self.rect.center+=direction
+    else:
+      self.offset = pygame.Vector2(0,0)
+
+    return self.image
+
+
+class Item(pygame.sprite.Sprite):
+  def __init__(self, x, y, image, scale, isAnimated):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = image
+    self.scale = scale
+    self.image = pygame.transform.scale(self.image, (self.image.get_width()*self.scale,self.image.get_height()*self.scale))
+    self.rect = self.image.get_rect()
+    self.rect.width = 32*self.scale
+    self.rect.center = (x,y)
+    self.isAnimated = isAnimated
+  
+  def update(self, screen, f, offset):
+    self.surf = pygame.Surface((self.image.get_width()*self.scale,self.image.get_height()*self.scale), pygame.SRCALPHA)
+    if self.isAnimated:
+      self.surf.blit(self.image, (0,0), (32*f*self.scale, 0, 32*self.scale, 32*self.scale))
+    else:
+      self.surf.blit(self.image, (0,0), (32*0*self.scale, 0, 32*self.scale, 32*self.scale))
+    self.rect.center += offset
+    screen.blit(self.surf, self.rect)
+    
--- main.py
+++ main.py
@@ -1,37 +1,86 @@
 import pygame
 from pytmx.util_pygame import load_pygame
-from SpriteHandler import Player, Tile
+from SpriteHandler import Player, BG, Item
 
 pygame.init()
 screen = pygame.display.set_mode((1920, 1080))
 pygame.display.toggle_fullscreen()
 
-def mainloop():
+#work on a system to place items on the background as coordinates rather than onto screen as causes placement to move depending on where the player is.
+#make a way to check which item is actually being picked up
+
+start = pygame.image.load('data/images/startButton.png').convert_alpha()
+titlescreenBG = pygame.image.load('data/images/titlescreen.png').convert()
+swordImg = pygame.image.load('data/images/sword.png').convert_alpha()
+player2Img = pygame.image.load('data/images/basicCharacter.png').convert_alpha()
+titlescreenBG = pygame.transform.scale(titlescreenBG, (titlescreenBG.get_width()*1.5,titlescreenBG.get_height()*1.5))
+start = pygame.transform.scale(start, (start.get_width()*4,start.get_height()*4))
+startRect = start.get_rect()
+startRect.center = (960,540)
+
+bgSurf = pygame.Surface((5000,5000), pygame.SRCALPHA)
+tmx = load_pygame('data/tiled/tmx/test.tmx')
+for layer in tmx.visible_layers:
+    if hasattr(layer,'data'):
+        for x, y, surf in layer.tiles():
+            pos = (x*16, y*16)
+            bgSurf.blit(surf, pos)
+
+background = BG(bgSurf)
+
+def mainloop(bgSurf):
     playerObj = Player(960,540,4)
     player = pygame.sprite.Group()
-    tileGroup = pygame.sprite.Group()
+    itemGroup = pygame.sprite.Group()
     clock = pygame.time.Clock()
-    f=0
-    time=0
-    orientation=0
-    isMoving=False
-    run=False
+    swordItem = Item(2000,500,swordImg,3,True)
+    player2 = Item(1920,1080,player2Img,10,False)
+    itemGroup.add(swordItem)
+    itemGroup.add(player2)
 
     player.add(playerObj)
 
-    tmx = load_pygame('data/tiled/tmx/test.tmx')
-    for layer in tmx.visible_layers:
-        if hasattr(layer,'data'):
-            for x, y, surf in layer.tiles():
-                pos = (x*16*5, y*16*5)
-                t = Tile(pos, surf, tileGroup)
+    walktime=0
+    walkf=0
+    atttime=0
+    attf=0
+    itemtime=0
+    itemf=0
+    orientation=0
+    playerSpeed = 4
+    isMoving=False
+    run=False
+    attacking=False
+    canAttack=True
+    swordHeld=False
 
-    start = pygame.image.load('data/images/startButton.png').convert_alpha()
-    titlescreenBG = pygame.image.load('data/images/titlescreen.png').convert()
-    titlescreenBG = pygame.transform.scale(titlescreenBG, (titlescreenBG.get_width()*1.5,titlescreenBG.get_height()*1.5))
-    start = pygame.transform.scale(start, (start.get_width()*4,start.get_height()*4))
-    startRect = start.get_rect()
-    startRect.center = (960,540)
+    def a_walk(walkf, swordHeld):
+        if swordHeld:
+            if walkf==7:
+                walkf=0
+            else:
+                walkf+=1
+        else:
+            if walkf==3:
+                walkf=0
+            else:
+                walkf+=1
+
+        return walkf
+    
+    def a_attack(attf):
+        if attf==3:
+            attf=0
+        else:
+            attf+=1
+        return attf
+    
+    def a_item(itemf):
+        if itemf==4:
+            itemf=0
+        else:
+            itemf+=1
+        return itemf
 
     titlescreen=True
     while titlescreen:
@@ -55,54 +104,88 @@ def mainloop():
         pygame.display.update()
 
     while run:
-        time+=1
         clock.tick(60)
-        screen.fill((0,0,0))
-        tileGroup.draw(screen)
-        tileGroup.update(isMoving)
+        screen.fill((127,205,255))
+        bgSurf = background.update(isMoving, playerSpeed)
+        screen.blit(bgSurf, (background.rect))
+        #pygame.draw.rect(screen, (255,0,0), playerObj.rect)
+        player.update(walkf, attf, screen, orientation, attacking, swordHeld)
+        itemGroup.update(screen, itemf, background.offset)
+
+        itemtime+=1
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w] or keys[pygame.K_a] or keys[pygame.K_s] or keys[pygame.K_d]:
+        if keys[pygame.K_a] and keys[pygame.K_d] or keys[pygame.K_w] and keys[pygame.K_s]:
+            isMoving=False
+        elif keys[pygame.K_w] or keys[pygame.K_a] or keys[pygame.K_s] or keys[pygame.K_d]:
+            if walktime==0:
+                walktime+=6
+            else:
+                walktime+=1
+            if walktime==12:
+                walkf = a_walk(walkf, swordHeld)
+                walktime=0
             isMoving=True
-            time+=1
         else:
             isMoving=False
-            time=0
 
-        if keys[pygame.K_a] and keys[pygame.K_d]:
-            isMoving=False
-            time=0
-        if keys[pygame.K_w] and keys[pygame.K_s]:
-            isMoving=False
-            time=0
-        elif keys[pygame.K_a]:
+
+        if keys[pygame.K_a]:
             orientation=1
         elif keys[pygame.K_d]:
             orientation=2
 
-        player.update(f, screen, orientation)
-        if time==12:
-            if f==3:
-                f=0
-            else:
-                f+=1
-            time=0
-        elif time==0:
-            f=0
+
+        if not isMoving:
+            walkf=1
+            walktime=0
+
+        if swordHeld:
+            if canAttack:
+                if pygame.mouse.get_pressed()[0]:
+                    attacking=True
+                    canAttack=False
+                elif pygame.mouse.get_pressed()[2]:
+                    swordHeld = False
+                    playerSpeed = 4
+                    walkf=0
+                    itemGroup.add(Item(2000,500,swordImg,3,True))
+            elif not pygame.mouse.get_pressed()[0]:
+                    canAttack=True
+
+
+        if attacking:
+            atttime+=1
+            if atttime == 3:
+                attf = a_attack(attf)
+                atttime=0
+                if attf==0:
+                    attacking=False
+
+        if itemtime==8:
+            itemtime=0
+            itemf = a_item(itemf)
+
+
+        itemPickedUp = pygame.sprite.groupcollide(player, itemGroup, False, True)
+        for item in itemPickedUp.values():
+            if item != None:
+                swordHeld = True
+                playerSpeed = 3
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run=False
-                mainloop()
+                mainloop(bgSurf)
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     run=False
-                    mainloop()
+                    mainloop(bgSurf)
             
         
         pygame.display.update()
 
-mainloop()
+mainloop(bgSurf)
 
 pygame.quit()
